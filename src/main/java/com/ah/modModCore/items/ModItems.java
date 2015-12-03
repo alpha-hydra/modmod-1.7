04aa714c20dfb907a25cc194bfbc0cab6053554e
alpha-hydra <alphahydra@outlook.com>
2015-12-03 14:51:54 +0000

    Added Creative Tabs

--- src/main/java/com/ah/modModCore/items/ModItems.java
+++ src/main/java/com/ah/modModCore/items/ModItems.java
@@ -21,20 +21,52 @@ public final class ModItems
 	
 	public static void init()
 	{
-		GameRegistry.registerItem(ingotCopper = new Item().setUnlocalizedName("ingotCopper").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":ingotCopper"), "ingotCopper");
-		GameRegistry.registerItem(ingotTin = new Item().setUnlocalizedName("ingotTin").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":ingotTin"), "ingotTin");
-		GameRegistry.registerItem(ingotAluminium = new Item().setUnlocalizedName("ingotAluminium").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":ingotAluminium"), "ingotAluminium");
-		GameRegistry.registerItem(ingotNickel = new Item().setUnlocalizedName("ingotNickel").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":ingotNickel"), "ingotNickel");
-		GameRegistry.registerItem(ingotTitanium = new Item().setUnlocalizedName("ingotTitanium").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":ingotTitanium"), "ingotTitanium");
-		GameRegistry.registerItem(ingotPlatinum = new Item().setUnlocalizedName("ingotPlatinum").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":ingotPlatinum"), "ingotPlatinum");
+		GameRegistry.registerItem(ingotCopper = new Item().setUnlocalizedName("ingotCopper").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":ingotCopper"), "ingotCopper");
+		GameRegistry.registerItem(ingotTin = new Item().setUnlocalizedName("ingotTin").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":ingotTin"), "ingotTin");
+		GameRegistry.registerItem(ingotAluminium = new Item().setUnlocalizedName("ingotAluminium").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":ingotAluminium"), "ingotAluminium");
+		GameRegistry.registerItem(ingotNickel = new Item().setUnlocalizedName("ingotNickel").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":ingotNickel"), "ingotNickel");
+		GameRegistry.registerItem(ingotTitanium = new Item().setUnlocalizedName("ingotTitanium").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":ingotTitanium"), "ingotTitanium");
+		GameRegistry.registerItem(ingotPlatinum = new Item().setUnlocalizedName("ingotPlatinum").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":ingotPlatinum"), "ingotPlatinum");
 		
-		GameRegistry.registerItem(dustNeodymium = new Item().setUnlocalizedName("dustNeodymium").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":dustNeodymium"), "dustNeodymium");
-		GameRegistry.registerItem(dustSulphur = new Item().setUnlocalizedName("dustSulphur").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(modModCore.modID + ":dustSulphur"), "dustSulphur");
+		GameRegistry.registerItem(dustNeodymium = new Item().setUnlocalizedName("dustNeodymium").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":dustNeodymium"), "dustNeodymium");
+		GameRegistry.registerItem(dustSulphur = new Item().setUnlocalizedName("dustSulphur").setCreativeTab(CreativeTabs.tabMaterials).setCreativeTab(CreativeTabs.tabModModMat).setTextureName(modModCore.modID + ":dustSulphur"), "dustSulphur");
 		
 		GameRegistry.registerItem(gear = new MetadataItem("gear", 9), "gear");
 		GameRegistry.registerItem(plate = new MetadataItem("plate", 9), "plate");
 		GameRegistry.registerItem(rod = new MetadataItem("rod", 9), "rod");
 		GameRegistry.registerItem(cylinder = new MetadataItem("cylinder", 9), "cylinder");
+	}
+	
+	public static final CreativeTabs tabModModMat = new CreativeTabs("ModModMat")
+	{
+		
+		@Override
+		public Item getTabIconItem()
+		{
+			
+			return this.ingotTitanium;
+			
+		}
+		
+	}
+	
+	public static final CreativeTabs tabModModComp = new CreativeTabs("ModModComp")
+	{
+		
+		@Override
+		public Item getTabIconItem()
+		{
+			
+			return this.gear;
+		}
+		
+		@Override
+		public int getItemIconDamage()
+		{
+			
+			return 6;
+			
+		}
 	}
 
 }
