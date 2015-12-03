3819fa96dd54dae7dc0bcb52a918c5832e165e0b
alpha-hydra <alphahydra@outlook.com>
2015-12-03 11:22:20 +0000

    Create OreDict.java

---
+++ src/main/java/com/ah/modModCore/core/OreDict.java
@@ -0,0 +1,29 @@
+package com.ah.modModCore.core;
+
+/* TODO
+ * Add Imports
+ */
+
+public static init()
+{
+
+	OreDictionary.RegisterOre("ingotCopper", modItems.ingotCopper)
+	OreDictionary.RegisterOre("ingotTin", modItems.ingotTin)
+	OreDictionary.RegisterOre("ingotNickel", modItems.ingotNickel)
+	OreDictionary.RegisterOre("ingotPlatinum", modItems.ingotPlatinum)
+	OreDictionary.RegisterOre("ingotAluminium", modItems.ingotAluminium)
+	OreDictionary.RegisterOre("ingotTitanium", modItems.ingotTitanium)
+	
+	OreDictionary.RegisterOre("ingotTitanium", modItems.ingotTitanium)
+	
+	OreDictionary.RegisterOre("oreCopper", modBlocks.oreCopper)
+	OreDictionary.RegisterOre("oreTin", modBlocks.oreTin)
+	OreDictionary.RegisterOre("oreNickel", modBlocks.oreNickel)
+	OreDictionary.RegisterOre("orePlatinum", modBlocks.orePlatinum)
+	OreDictionary.RegisterOre("oreAluminium", modBlocks.oreAluminium)
+	OreDictionary.RegisterOre("oreTitanium", modBlocks.oreTitanium)
+	OreDictionary.RegisterOre("oreNeodymium", modBlocks.oreNeodymium)
+	OreDictionary.RegisterOre("oreSulphur", modBlocks.oreSulphur)
+	OreDictionary.RegisterOre("oreSulfur", modBlocks.oreSulphur)
+
+}
